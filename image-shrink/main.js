fd431ca3143afc21713623d5beeecde14e4d5e1f
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-01-02 01:32:28 +0600

    app initialized, html rendered as view, icon shown

---
+++ image-shrink/main.js
@@ -0,0 +1,16 @@
+const {app, BrowserWindow} = require("electron");
+
+let mainWindow;
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        title: "Image Shrink",
+        width: 500,
+        height: 600,
+        icon: "./assets/icons/Icon_256x256.png"
+    });
+
+    mainWindow.loadFile(`./app/index.html`);
+}
+
+app.on("ready", createMainWindow);
