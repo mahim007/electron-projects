81319401ab3aa81603ca925bf1ad07cd6d8c1934
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-01-04 01:15:18 +0600

    image shrik app completed, packaging done

---
+++ image-shrink/app/js/renderer.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const os = require("os");
+const { ipcRenderer } = require("electron");
+
+const form = document.getElementById("image-form");
+const slider = document.getElementById("slider");
+const img = document.getElementById("img");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const imgPath = img.files[0].path;
+  const quality = slider.value;
+
+  console.log(imgPath, quality);
+  ipcRenderer.send("image:minimize", {
+    imgPath,
+    quality,
+  });
+});
+
+ipcRenderer.on("image:done", () => {
+  M.toast({
+    html: `Imge resized to ${slider.value}% quality`,
+  });
+});
+
+document.getElementById("output-path").innerText = path.join(
+  os.homedir(),
+  "imageshrink"
+);
--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,16 +1,147 @@
-const {app, BrowserWindow} = require("electron");
+const path = require("path");
+const os = require("os");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const imageminPngquant = require("imagemin-pngquant");
+const imagemin = require("imagemin");
+const slash = require("slash");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const log = require("electron-log");
+
+process.env.NODE_ENV = "production";
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+console.log(process.platform);
+const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
-    mainWindow = new BrowserWindow({
-        title: "Image Shrink",
-        width: 500,
-        height: 600,
-        icon: "./assets/icons/Icon_256x256.png"
-    });
+  mainWindow = new BrowserWindow({
+    title: "Image Shrink",
+    width: 500,
+    height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev ? true : false,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    mainWindow.loadFile(`./app/index.html`);
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile(`./app/index.html`);
 }
 
-app.on("ready", createMainWindow);
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: "About Image Shrink",
+    width: 400,
+    height: 300,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: false,
+    backgroundColor: "white",
+  });
+
+  aboutWindow.loadFile("./app/about.html");
+}
+
+app.on("ready", () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("ready", () => (mainWindow = null));
+});
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: "fileMenu",
+  },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forceReload" },
+            { role: "toggleDevTools" },
+            { type: "separator" },
+            { role: "resetZoom" },
+            { role: "zoomIn" },
+            { role: "zoomOut" },
+            { type: "separator" },
+            { role: "togglefullscreen" },
+          ],
+        },
+      ]
+    : []),
+];
+
+ipcMain.on("image:minimize", (e, options) => {
+  options.dest = path.join(os.homedir(), "imageshrink");
+  shrinkImage(options);
+});
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+    const files = await imagemin([slash(imgPath)], {
+      destination: slash(dest),
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    });
+
+    log.info(imgPath, quality, dest);
+
+    shell.openPath(dest);
+    mainWindow.webContents.send("image:done");
+  } catch (error) {
+    log.error(error);
+  }
+}
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
